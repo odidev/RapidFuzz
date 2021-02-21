7bbf3e23829a5edae84ede39214b09eb7128fd56
Max Bachmann <kontakt@maxbachmann.de>
2021-02-22 00:47:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
         'msvc': ['/EHsc', '/O2', '/std:c++11'],
-        'unix': ['-O3', '-std=c++11', '-Wextra', '-Wall',  '-mtune=generic', '-fasynchronous-unwind-tables'],
+        'unix': ['-O3', '-std=c++11', '-Wextra', '-Wall',  '-mtune=generic', '-fasynchronous-unwind-tables', '-m64'],
     }
     l_opts = {
         'msvc': [],
